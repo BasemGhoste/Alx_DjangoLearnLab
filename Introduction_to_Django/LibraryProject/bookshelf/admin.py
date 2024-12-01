7143585aa1874d0aecca34b854e01c0187fa6ceb
BasemGhoste <https://github.com/BasemGhoste/alx_be_python.git>
2024-12-01 02:47:09 +0200

    task1

--- Introduction_to_Django/LibraryProject/bookshelf/admin.py
+++ Introduction_to_Django/LibraryProject/bookshelf/admin.py
@@ -1,4 +1,12 @@
 from django.contrib import admin
 from .models import Book
 
-admin.site.register(Book)
+
+@admin.register(Book)
+class BookAdmin(admin.ModelAdmin):
+    # Display these fields in the list view
+    list_display = ('title', 'author', 'publication_year')
+    # Enable search functionality for these fields
+    search_fields = ('title', 'author')
+    # Add filters for these fields
+    list_filter = ('publication_year',)
